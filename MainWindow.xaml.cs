03509ab30a4fd9620103864a84da96b3da32ff26
zengqiqi <791928163@qq.com>
2017-03-24 14:24:11 +0800

    firstcommit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApplication1.dbl;
+
+namespace WpfApplication1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            //HttpHeader header = new HttpHeader();
+            //header.accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/x-silverlight, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-ms-application, application/x-ms-xbap, application/vnd.ms-xpsdocument, application/xaml+xml, application/x-silverlight-2-b1, */*";
+            ////header.accept = "*/*";
+            //header.contentType = "application/x-www-form-urlencoded";
+            //header.method = "POST";
+            ////header.userAgent = "Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1; .NET CLR 2.0.50727; .NET CLR 3.0.04506.648; .NET CLR 3.5.21022)";
+            //header.userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            //header.maxTry = 300;
+
+
+            //CookieCollection mycookie = HTMLHelper.GetCookieCollection("https://www.douban.com/accounts/login",
+            //                   "source=index_nav&form_email=791928163%40qq.com&form_password=z123456789", header);
+            ////__EVENTTARGET=btnLogin&__EVENTARGUMENT=&__VIEWSTATE=EkHCjgCKu6Pz4LjCikKuW%2FEEvatJu6%2Fo7uixsMmpK7MotDc%2FkR9I2K%2BJylTPkdk2EdQXsOck%2Fhf9t%2BJpED1M1uEEHRtj9n%2FnmoLaeBs7Lz0%3D&__VIEWSTATEGENERATOR=C2EE9ABB&__EVENTVALIDATION=R1Miao38A8tauHdJtZPQCjD%2FGAh%2FW7DREKWmflHnEK3baCw7MQwP76sL%2FuN1zfZes9wREYp7HdXX421ZFi0b7rwyAEkJs5pp%2BnmQlDKVAye8M1lSFxPTNtmomdrpebDnn%2BOWUl2IfB%2BCtCQ1Sb6Nu9D4qNQ8PQ6I6TJCttpv8TE%3D&txtUser=admin&txtPassword=admin
+            ////source=index_nav&redir=https%3A%2F%2Fwww.douban.com%2F&form_email=791928163%40qq.com&form_password=z123456789&remember=on&login=%E7%99%BB%E5%BD%95
+            //foreach (Cookie cookie in mycookie) //将cookie设置为浏览的cookie  
+            //{
+
+            //    InternetSetCookie(
+
+            //         "http://" + cookie.Domain.ToString(),
+
+            //         cookie.Name.ToString(),
+
+            //         cookie.Value.ToString() + ";expires=Sun,22-Feb-2099 00:00:00 GMT");
+
+            //}
+            //System.Diagnostics.Process.Start("https://www.douban.com/");
+
+
+            HttpHeader header = new HttpHeader();
+            header.accept = "image/gif, image/x-xbitmap, image/jpeg, image/pjpeg, application/x-shockwave-flash, application/x-silverlight, application/vnd.ms-excel, application/vnd.ms-powerpoint, application/msword, application/x-ms-application, application/x-ms-xbap, application/vnd.ms-xpsdocument, application/xaml+xml, application/x-silverlight-2-b1, */*";
+            header.contentType = "application/x-www-form-urlencoded";
+            header.method = "POST";
+            header.userAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/53.0.2785.116 Safari/537.36";
+            header.maxTry = 300;
+            header.host = "www.douban.com";
+            header.referer = "https://www.douban.com/";
+
+            string html = HTMLHelper.GetHtml("https://www.douban.com/", HTMLHelper.GetCooKie("https://www.douban.com/accounts/login",
+                "source=index_nav&form_email=791928163%40qq.com&form_password=z123456789", header), header);
+
+            txtresult.Text = html;
+
+            //Console.WriteLine(html);
+
+
+            //Console.ReadLine();
+        }
+
+        [DllImport("wininet.dll", CharSet = CharSet.Auto, SetLastError = true)]
+
+        public static extern bool InternetSetCookie(string lpszUrlName, string lbszCookieName, string lpszCookieData);
+
+
+        
+    }
+}
---
+++ dbl/HTMLHelper.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WpfApplication1.dbl
+{
+    public class HTMLHelper
+    {
+        /// <summary>
+        /// 获取CooKie
+        /// </summary>
+        /// <param name="loginUrl"></param>
+        /// <param name="postdata"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static CookieContainer GetCooKie(string loginUrl, string postdata, HttpHeader header)
+        {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            try
+            {
+                CookieContainer cc = new CookieContainer();
+                request = (HttpWebRequest)WebRequest.Create(loginUrl);
+                request.Method = header.method;
+                request.ContentType = header.contentType;
+                byte[] postdatabyte = Encoding.UTF8.GetBytes(postdata);
+                request.ContentLength = postdatabyte.Length;
+                request.AllowAutoRedirect = false;
+                request.CookieContainer = cc;
+                request.KeepAlive = true;
+                request.Host = header.host;
+                request.Referer = header.referer;
+
+                //request.Date = DateTime.Now;
+                //request.Headers.Add("Upgrade-Insecure-Requests", "1");
+                //request.Headers.Add("Cookie", "bid=x4IcfRm_T5k; ll=\"108296\"; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1488267551%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3Dj_A_XzKpi1jthHtOTUIWUQXX3Y_mL97c16RrnIxTjajsIlceLC03olEEO-ERCxi3%26wd%3D%26eqid%3Deb4c0e4000025fd50000000458b52923%22%5D; __utmt=1; regpop=1; ct=y; _pk_id.100001.8cb4=d890162580033b8e.1488267551.1.1488267881.1488267551.; _pk_ses.100001.8cb4=*; __utma=30149280.39037576.1488267552.1488267552.1488267552.1; __utmb=30149280.4.10.1488267552; __utmc=30149280; __utmz=30149280.1488267552.1.1.utmcsr=baidu|utmccn=(organic)|utmcmd=organic");
+                //request.Headers.Add("Origin", "https://www.douban.com");
+                //request.Headers.Add("Cache-Control", "max-age=0");
+                //request.Headers.Add("Accept-Language", "cs,en-us;q=0.7,en;q=0.3");
+                //request.Headers.Add("Accept-Encoding", "gzip, deflate");
+                //request.Accept = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8";
+
+                //提交请求
+                Stream stream;
+                stream = request.GetRequestStream();
+                stream.Write(postdatabyte, 0, postdatabyte.Length);
+                stream.Close();
+
+                //接收响应
+                response = (HttpWebResponse)request.GetResponse();
+                response.Cookies = request.CookieContainer.GetCookies(request.RequestUri);
+
+                CookieCollection cook = response.Cookies;
+                //Cookie字符串格式
+                string strcrook = request.CookieContainer.GetCookieHeader(request.RequestUri);
+
+                return cc;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// 获取html
+        /// </summary>
+        /// <param name="getUrl"></param>
+        /// <param name="cookieContainer"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static string GetHtml(string getUrl, CookieContainer cookieContainer, HttpHeader header)
+        {
+            Thread.Sleep(1000);
+            HttpWebRequest httpWebRequest = null;
+            HttpWebResponse httpWebResponse = null;
+            try
+            {
+                httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(getUrl);
+                httpWebRequest.CookieContainer = cookieContainer;
+                httpWebRequest.ContentType = header.contentType;
+                httpWebRequest.ServicePoint.ConnectionLimit = header.maxTry;
+                httpWebRequest.Referer = getUrl;
+                httpWebRequest.Accept = header.accept;
+                httpWebRequest.UserAgent = header.userAgent;
+                httpWebRequest.Method = "GET";
+                httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                Stream responseStream = httpWebResponse.GetResponseStream();
+                StreamReader streamReader = new StreamReader(responseStream, Encoding.UTF8);
+                string html = streamReader.ReadToEnd();
+                streamReader.Close();
+                responseStream.Close();
+                httpWebRequest.Abort();
+                httpWebResponse.Close();
+                return html;
+            }
+            catch (Exception e)
+            {
+                if (httpWebRequest != null) httpWebRequest.Abort();
+                if (httpWebResponse != null) httpWebResponse.Close();
+                return string.Empty;
+            }
+        }
+
+        /// <summary>
+        /// 获取CookieCollection
+        /// </summary>
+        /// <param name="loginUrl"></param>
+        /// <param name="postdata"></param>
+        /// <param name="header"></param>
+        /// <returns></returns>
+        public static CookieCollection GetCookieCollection(string loginUrl, string postdata, HttpHeader header)
+        {
+            HttpWebRequest request = null;
+            HttpWebResponse response = null;
+            try
+            {
+                CookieContainer cc = new CookieContainer();
+                request = (HttpWebRequest)WebRequest.Create(loginUrl);
+                request.Method = header.method;
+                request.ContentType = header.contentType;
+                byte[] postdatabyte = Encoding.UTF8.GetBytes(postdata);
+                request.ContentLength = postdatabyte.Length;
+                request.AllowAutoRedirect = false;
+                request.CookieContainer = cc;
+                request.KeepAlive = true;
+
+                //提交请求
+                Stream stream;
+                stream = request.GetRequestStream();
+                stream.Write(postdatabyte, 0, postdatabyte.Length);
+                stream.Close();
+
+                //接收响应
+                response = (HttpWebResponse)request.GetResponse();
+                response.Cookies = request.CookieContainer.GetCookies(request.RequestUri);
+
+                CookieCollection cook = response.Cookies;
+                //Cookie字符串格式
+                string strcrook = request.CookieContainer.GetCookieHeader(request.RequestUri);
+
+                return cook;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+
+    public class HttpHeader
+    {
+        public string contentType { get; set; }
+
+        public string accept { get; set; }
+
+        public string userAgent { get; set; }
+
+        public string method { get; set; }
+
+        public int maxTry { get; set; }
+
+        public string referer { get; set; }
+
+        public string host { get; set; }
+
+        public string cookie { get; set; }
+    }
+}
